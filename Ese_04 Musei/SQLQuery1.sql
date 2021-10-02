c90edf83e56ed8719e1537e3aaaf4cb95d1daf41
ivan-angjelovski <62544677+ivan-angjelovski@users.noreply.github.com>
2021-10-02 11:20:20 +0200

    Updated Es04

--- Ese_04 Musei/SQLQuery1.sql
+++ Ese_04 Musei/SQLQuery1.sql
@@ -25,45 +25,45 @@ AND NOT EXISTS(SELECT *
 
 /* 11. Per ciascun artista, il nome dell'artista ed il numero di sue opere conservate alla “Galleria
 degli Uffizi” */
-SELECT o.NomeArtista,COUNT(*) as NOpere
+SELECT o.NomeA,COUNT(*) as NOpere
 FROM Opere o
-WHERE o.NomeMuseo='Galleria degli Uffizi'
-GROUP BY o.NomeArtista  
+WHERE o.NomeM='Galleria degli Uffizi'
+GROUP BY o.NomeA  
 
 
 /* 12. I musei che conservano almeno 20 opere di artisti italiani */
-SELECT o.NomeMuseo, COUNT(*) as n
+SELECT o.NomeM, COUNT(*) as n
 FROM Opere o, Artisti a
-WHERE o.NomeArtista=a.NomeArtista
+WHERE o.NomeA=a.NomeA
 AND a.Nazionalita='IT'
-GROUP BY o.NomeMuseo
+GROUP BY o.NomeM
 HAVING COUNT(*)>1
 
 
 
 /* 13- Per le opere di artisti italiani che non hanno personaggi. il titolo dell’opera ed il nome
 dell'artista */
-SELECT a.NomeArtista, o.Titolo
+SELECT a.NomeA, o.Titolo
 FROM Artisti a, Personaggi p, Opere o
 WHERE a.Nazionalita='IT'
 AND p.Personaggio=NULL
-AND p.Codice=o.Codice AND a.NomeArtista=o.NomeArtista
+AND p.Codice=o.Codice AND a.NomeA=o.NomeA
 
 /* 14- Il nome dei musei di Londra che non conservano opere di artisti italiani, eccettuato Tiziano */
 SELECT *
 FROM Opere o, Musei m
-WHERE o.NomeMuseo=m.NomeMuseo
+WHERE o.NomeM=m.NomeM
 AND m.Citta='Londra'
 AND NOT EXISTS(SELECT *
 			FROM Artisti a
-			WHERE a.NomeArtista=o.NomeArtista
-			AND a.NomeArtista<>'Tiziano' 
+			WHERE a.NomeA=o.NomeA
+			AND a.NomeA<>'Tiziano' 
 			AND a.Nazionalita='IT')
 
 
 /* 15- Per ogni museo, il numero di opere divise per la nazionalità dell'artista */
-SELECT o.NomeMuseo,a.Nazionalita, COUNT(*) AS nOpere
+SELECT o.NomeM,a.Nazionalita, COUNT(*) AS nOpere
 FROM Artisti a, Opere o
-WHERE a.NomeArtista=o.NomeArtista
-GROUP BY o.NomeMuseo,a.Nazionalita
-ORDER BY o.NomeMuseo
+WHERE a.NomeA=o.NomeA
+GROUP BY o.NomeM,a.Nazionalita
+ORDER BY o.NomeM
